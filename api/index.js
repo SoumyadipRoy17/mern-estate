cc9ade9ad531d55fe9033d9bf9c812256342c432
Soumyadip Roy <email@gmail.com>
2023-11-23 18:30:08 +0530

    create sign up api route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,8 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
+
 dotenv.config();
 
 const app = express();
@@ -14,8 +16,11 @@ mongoose
     console.log(err);
   });
 
+app.use(express.json());
+
 app.listen(3000, () => {
   console.log("Server is running on port 3000 🚀");
 });
 
 app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
